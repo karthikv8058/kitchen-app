833e8f6734869ed0e4e1b02f560c739e3c1281c7
Nooh K V M <nooh.km@pitsolutions.com>
2021-04-22 12:42:26 +0530

    typo refactor

--- android/app/src/main/java/com/smarttoni/guestgroup/entities/GuestGroup.kt
+++ android/app/src/main/java/com/smarttoni/guestgroup/entities/GuestGroup.kt
@@ -3,4 +3,4 @@ package com.smarttoni.guestgroup.entities
 import androidx.room.Entity
 
 @Entity
-class GuestGroup(var name: Int,var station: String,var room: String,var table: String)
+class GuestGroup(var name: Int,var room: String,var station: String,var table: String)
